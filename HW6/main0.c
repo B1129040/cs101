22e8302d4279a50c612faa63d08188eb909986d5
B1129040 <119659262+B1129040@users.noreply.github.com>
2022-12-22 15:47:33 +0800

    Create main0.c

---
+++ HW6/main0.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+
+int i = 10;
+    if(i&i-1){
+        printf("%d is False \n",i);
+    }
+    else{
+        printf("%d is True \n",i);
+    }
+   return 0;
+}
