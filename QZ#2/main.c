82a66e04cfc9a3de12b00028c36308da7cf34703
B1129040 <119659262+B1129040@users.noreply.github.com>
2022-12-29 16:50:56 +0800

    Update main.c

--- QZ#2/main.c
+++ QZ#2/main.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
 
+int main()
+{
+    int rows = 5;
+    void print_spaces(int rows, int r){
+        for (int space =0 ; space <= rows -r; space++);{
+        printf(" ");
+        int i;
+        }
+}
+
+void print_stars(int r){
+    for (int i=0; i<n; i++){
+        printf("%d ", n);
+    }
+    printf("\n");
+    int i;
+}
+
+int main() {
+    int rows = 5
+    int j;
+    for (int i = i>=1; --i){
+        print_space(i);
+        print_stars(i);
+    }
+    
+    return 0;
+}
